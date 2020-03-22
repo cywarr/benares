fdf58d87bd9a183f7c26b64977c6d834d95b18e2
cywarr <prisoner849@mail.ru>
2020-03-23 00:37:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitCo
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 0.1, 100);
-camera.position.set(1, 2, 3).setLength(4);
+camera.position.set(0, 0, 3).setLength(4);
 var renderer = new THREE.WebGLRenderer({
     antialias: true
 });
@@ -29,10 +29,10 @@ var reflectionCube = cubeTextureLoader.load(urls);
 
 //scene.background = reflectionCube;
 
-/*var light = new THREE.DirectionalLight(0xffffff, 0.125);
+var light = new THREE.DirectionalLight(0xffffff, 0.75);
 light.position.set(0, 1, 0);
-scene.add(light);*/
-scene.add(new THREE.AmbientLight(0xffffff, 1));
+scene.add(light);
+scene.add(new THREE.AmbientLight(0xffffff, 0.25));
 
 //scene.add(new THREE.GridHelper(10, 10));
 
@@ -45,11 +45,7 @@ var corpuscules = [];
 var sphereColor = 0x4488ff;
 var sGeom = new THREE.SphereBufferGeometry(0.05, 16, 16);
 var sMat = new THREE.MeshStandardMaterial({
-    color: sphereColor,
-    envMap: reflectionCube,
-    envMapIntensity: 0.5,
-    roughness: 0.,
-    metalness: 0.75
+    color: sphereColor
 });
 for (let i = 0; i < spheresAmount; i++) {
     let sphere = new THREE.Mesh(sGeom, sMat);
@@ -79,40 +75,12 @@ for (let i = 0; i < sPos.count; i++){
     sNorm.setXYZ(i, temp.x, temp.y, temp.z);
 }
 mainSphereGeom = mainSphereGeom.toNonIndexed();
-//console.log(mainSphereGeom, mainSphereGeom.attributes.position.count);
 
-// vertices of a face before and after
-var vertAfter = [];
-var vecAfter = new THREE.Vector3();
-var vertBefore = [];
-var vecBefore = new THREE.Vector3();
-
-var pos = mainSphereGeom.attributes.position;
-for (let i = 0; i < pos.count; i++) {
-    let face = Math.floor(i / 3);
-
-    let afterIdx = (i % 3) + 1;
-    afterIdx = afterIdx > 2 ? 0 : afterIdx;
-    vecAfter.fromBufferAttribute(pos, (face * 3) + afterIdx);
-    vertAfter.push(vecAfter.x, vecAfter.y, vecAfter.z);
-
-    let beforeIdx = (i % 3) - 1;
-    beforeIdx = beforeIdx < 0 ? 2 : beforeIdx;
-    vecBefore.fromBufferAttribute(pos, (face * 3) + beforeIdx);
-    vertBefore.push(vecBefore.x, vecBefore.y, vecBefore.z)
-}
-mainSphereGeom.setAttribute("vertAfter", new THREE.Float32BufferAttribute(vertAfter, 3));
-mainSphereGeom.setAttribute("vertBefore", new THREE.Float32BufferAttribute(vertBefore, 3));
-
-//scene.background = reflectionCube;
 
 var mainSphereMat = new THREE.MeshStandardMaterial({
-    color: sphereColor,
-    envMap: reflectionCube,
-    envMapIntensity: 0.25,
-    metalness: 0.75,
-    roughness: 0,
-    wireframe: false
+    //color: 0x8888ff,
+    //envMap: reflectionCube,
+    map: new THREE.TextureLoader().load("https://threejs.org/examples/textures/uv_grid_opengl.jpg")
 });
 //mainSphereMat.defines = {"USE_ENVMAP":""};
 var uniforms = {
@@ -122,42 +90,44 @@ var uniforms = {
 };
 mainSphereMat.onBeforeCompile = shader => {
     shader.uniforms.corpuscules = uniforms.corpuscules;
-    console.log(shader.vertexShader);
-    //console.log(shader.fragmentShader);
     shader.vertexShader = `
   	uniform vec3 corpuscules[${spheresAmount}];
-    attribute vec3 vertAfter;
-    attribute vec3 vertBefore;
-
-    vec3 getShiftedPoint(vec3 p){
-      vec3 accumulate = vec3(0);
-
-      for(int i = 0; i < ${spheresAmount}; i++){
-        vec3 sPos = corpuscules[i];
-        vec3 diff = sPos - p;
-        vec3 dir = normalize(diff);
-        float dist = length(diff);
-
-        float force = .0125 / pow(dist, 1.);
-        vec3 forceVec = dir * clamp(force, 0., dist);
-
-        accumulate += forceVec;
-      }
-      return p + accumulate;
-    }
   ` + shader.vertexShader;
     shader.vertexShader = shader.vertexShader.replace(
         `#include <begin_vertex>`,
         `#include <begin_vertex>
- 
-    transformed = getShiftedPoint(transformed);
+    
+    vec3 accumulate = vec3(0);
+    float shortestDist = 1000.;
+    
+    for(int i = 0; i < ${spheresAmount}; i++){
+    	vec3 sPos = corpuscules[i];
+      vec3 diff = sPos - transformed;
+      vec3 dir = normalize(diff);
+      float dist = length(diff);
+      shortestDist = min(shortestDist, dist);
+      
+      float force = .00625 / (dist * dist);
+      vec3 forceVec = dir * force;
+      
+      accumulate += forceVec;
+    }
+    
+    vec3 normAccumulate = normalize(accumulate);
+    
+    float accumulateLength = clamp(length(accumulate), 0., shortestDist);
+    accumulate = normAccumulate * accumulateLength;
 
-    vec3 pointAfter = getShiftedPoint(vertAfter);
-    vec3 pointBefore = getShiftedPoint(vertBefore);
+    float distRatio = accumulateLength / shortestDist;
 
-    vec3 cb = normalize(pointAfter - transformed);
-    vec3 ab = normalize(pointBefore - transformed);
-    transformedNormal = cross(cb, ab);
+    transformed += accumulate;
+   
+    // re-compute normals    
+    vec3 n0 = vec3(normal);
+    vec3 n1 = cross(normAccumulate, n0);
+    vec3 n2 = cross(n1, normAccumulate);
+    vec3 finalNormal = mix(n0, n2, distRatio);
+    transformedNormal = normalMatrix * finalNormal;
     `
     );
 }
