493e456623b5f0b4fa4a99347fd95b8cd4e02dfd
cywarr <prisoner849@mail.ru>
2020-03-25 00:51:54 +0300

    textures aum benares

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitCo
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, innerWidth / innerHeight, 0.1, 100);
-camera.position.set(0, 0, 3).setLength(4);
+camera.position.set(3, 3, 3).setLength(3.75);
 var renderer = new THREE.WebGLRenderer({
     antialias: true
 });
@@ -27,6 +27,10 @@ var cubeTextureLoader = new THREE.CubeTextureLoader();
 
 var reflectionCube = cubeTextureLoader.load(urls);
 
+var textureLoader = new THREE.TextureLoader();
+var texAum = textureLoader.load("./textures/aum.png");
+var texBenares = textureLoader.load("./textures/benares.png");
+
 //scene.background = reflectionCube;
 
 var light = new THREE.DirectionalLight(0xffffff, 0.125);
@@ -42,21 +46,19 @@ var angleStep = Math.PI / spheresAmount;
 var spheres = [];
 var corpuscules = [];
 
-var sphereColor = 0x222244;
-var sGeom = new THREE.SphereBufferGeometry(0.05, 16, 16);
-var sMat = new THREE.MeshStandardMaterial({
-    color: sphereColor
+var sphereColor = 0x884444;
+var sGeom = new THREE.SphereBufferGeometry(0.075, 16, 16);
+var sMat = new THREE.MeshLambertMaterial({
+    color: sphereColor,
+    envMap: reflectionCube,
+    reflectivity: 0.125
 });
 
 var icosahedronGeom = new THREE.IcosahedronGeometry(1, 0);
 for (let i = 0; i < spheresAmount; i++) {
     let sphere = new THREE.Mesh(sGeom, sMat);
-    sphere.userData.dirVector = new THREE.Vector3().copy(icosahedronGeom.vertices[i]);//.setFromSphericalCoords(
-        //1,
-        //(Math.random() - 0.5) < 0 ? (Math.random() * Math.PI * 0.25) : Math.PI - (Math.random() * Math.PI * 0.25), 
-        //Math.random() * Math.PI, 
-        //Math.random() * Math.PI * 2);
-    sphere.userData.dirTheta = Math.random() * Math.PI; //i * angleStep;
+    sphere.userData.dirVector = new THREE.Vector3().copy(icosahedronGeom.vertices[i]);
+    sphere.userData.dirTheta = Math.random() * Math.PI;
     spheres.push(sphere);
     scene.add(sphere);
     corpuscules.push(sphere.position);
@@ -68,6 +70,7 @@ var mainSphereGeom = new THREE.BoxBufferGeometry(2, 2, 2, 50, 50, 50);
 var sPos = mainSphereGeom.attributes.position;
 var sNorm = mainSphereGeom.attributes.normal;
 var temp = new THREE.Vector3();
+var sides = [];
 for (let i = 0; i < sPos.count; i++){
 
     temp.fromBufferAttribute(sPos, i);
@@ -75,33 +78,43 @@ for (let i = 0; i < sPos.count; i++){
 
     sPos.setXYZ(i, temp.x, temp.y, temp.z);
     sNorm.setXYZ(i, temp.x, temp.y, temp.z);
+    sides.push(Math.floor(i / (51 * 51)));
 }
-mainSphereGeom = mainSphereGeom.toNonIndexed();
+//mainSphereGeom = mainSphereGeom.toNonIndexed();
+mainSphereGeom.setAttribute("sides", new THREE.Float32BufferAttribute(sides, 1));
 
 
 var mainSphereMat = new THREE.MeshLambertMaterial({
     color: 0x222244,
     envMap: reflectionCube,
     reflectivity: 0.25
-    //envMapIntensity: 10,
-    //map: new THREE.TextureLoader().load("https://threejs.org/examples/textures/uv_grid_opengl.jpg"),
-    //metalness: 1,
-    //roughness: 0.25
 });
-mainSphereMat.defines = {"USE_ENVMAP":""};
+mainSphereMat.defines = {"USE_UV":""};
 var uniforms = {
     corpuscules: {
         value: corpuscules
+    },
+    texAum: {
+        value: texAum
+    },
+    time: {
+        value: 0
     }
 };
 mainSphereMat.onBeforeCompile = shader => {
     shader.uniforms.corpuscules = uniforms.corpuscules;
+    shader.uniforms.texAum = uniforms.texAum;
+    shader.uniforms.time = uniforms.time;
     shader.vertexShader = `
-  	uniform vec3 corpuscules[${spheresAmount}];
+      uniform vec3 corpuscules[${spheresAmount}];
+      attribute float sides;
+      varying float vSides;
   ` + shader.vertexShader;
     shader.vertexShader = shader.vertexShader.replace(
         `#include <begin_vertex>`,
         `#include <begin_vertex>
+
+    vSides = sides;
     
     vec3 accumulate = vec3(0);
     float shortestDist = 1000.;
@@ -136,6 +149,35 @@ mainSphereMat.onBeforeCompile = shader => {
     transformedNormal = normalMatrix * finalNormal;
     `
     );
+    
+    shader.fragmentShader = `
+        uniform sampler2D texAum;
+        uniform float time;
+        varying float vSides;
+    ` + shader.fragmentShader;
+    shader.fragmentShader = shader.fragmentShader.replace(
+        `#include <dithering_fragment>`,
+        `#include <dithering_fragment>
+        
+        float texVal = texture2D( texAum, vUv ).r;
+        
+        vec2 uv = vUv;
+        uv -= 0.5;
+        uv *= 20.25;
+
+        float a = atan(uv.x,uv.y)+PI;
+        float r = PI2/floor(1. + floor(mod(time + vSides, 6.)));
+        float d = cos(floor(.5+a/r)*r-a)*length(uv);
+
+        float waveVal = sin((d - time) * PI2) * 0.5 + 0.5;
+
+        vec3 col = vec3(0);
+        col = vec3(0, 0.5, 1) * 0.5;
+        //col = mix(col, vec3(0.5, 0.25, 0), waveVal);
+        gl_FragColor.rgb = mix(gl_FragColor.rgb, col, texVal * waveVal);
+        `
+    );
+    //console.log(shader.fragmentShader);
 }
 var mainSphere = new THREE.Mesh(mainSphereGeom, mainSphereMat);
 scene.add(mainSphere);
@@ -151,6 +193,7 @@ renderer.setAnimationLoop(() => {
         s.position.copy(s.userData.dirVector).multiplyScalar(Math.sin(s.userData.dirTheta + t) * 2);
 
     });
+    uniforms.time.value = t;
     renderer.render(scene, camera)
 });
 
